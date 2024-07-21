aa779920311f3ce5f4134db4936404946e5c95af
smarthidroponikMERAX <smarthidroponikmerax@gmail.com>
2024-07-22 00:02:47 +0700

    Update webtes.py

--- webtes.py
+++ webtes.py
@@ -527,16 +527,16 @@ def ml():
       st.write(f"Recall Random Forest: {recall_RF_nutrisi}")
       st.write(f"F1-score Random Forest: {f1_RF_nutrisi}")
 
-    st.subheader("Grafik Sensor")
+      st.subheader("Grafik Sensor")
     
-    st.write("pH Sensor:")
-    st.line_chart(data_rapi.set_index('waktu')['pH'])
+      st.write("pH Sensor:")
+      st.line_chart(data_rapi.set_index('waktu')['pH'])
 
-    st.write("Suhu Air:")
-    st.line_chart(data_rapi.set_index('waktu')['suhu'])
+      st.write("Suhu Air:")
+      st.line_chart(data_rapi.set_index('waktu')['suhu'])
 
-    st.write("Nutrisi (TDS):")
-    st.line_chart(data_rapi.set_index('waktu')['tds'])
+      st.write("Nutrisi (TDS):")
+      st.line_chart(data_rapi.set_index('waktu')['tds'])
 
 
 if __name__ == "__main__":
