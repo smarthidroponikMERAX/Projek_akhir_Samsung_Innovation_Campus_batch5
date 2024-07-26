9701c3fab7021559f8aaad9d664b6b00f41fa417
smarthidroponikMERAX <smarthidroponikmerax@gmail.com>
2024-07-26 22:38:00 +0700

    Update web.py

--- web.py
+++ web.py
@@ -532,22 +532,23 @@ def ml():
 if __name__ == "__main__":
         streamlit_app()
 
-# Menentukan halaman yang aktif
+        # Menentukan halaman yang aktif
         if 'page' not in st.session_state:
             st.session_state.page = "Home"
-            # Definisi halaman Home
-            if st.session_state.page == "Home":
-                st.title("Home")
-                st.write("Ini adalah halaman Home.")
-                if st.button("Lanjutkan ke Page 2"):
-                    st.session_state.page = "Page 2"
-                    
-            
-            # Definisi halaman Page 2
-            elif st.session_state.page == "Page 2":
-                st.title("Page 2")
-                ml()
-                st.write("Ini adalah halaman kedua.")
-                if st.button("Kembali ke Home"):
-                    st.session_state.page = "Home"
+        
+        # Definisi halaman Home
+        if st.session_state.page == "Home":
+            st.title("Home")
+            st.write("Ini adalah halaman Home.")
+            if st.button("Lanjutkan ke Page 2"):
+                st.session_state.page = "Page 2"
+        
+        # Definisi halaman Page 2
+        elif st.session_state.page == "Page 2":
+            st.title("Page 2")
+            ml()  # Panggil fungsi ml() di sini
+            st.write("Ini adalah halaman kedua.")
+            if st.button("Kembali ke Home"):
+                st.session_state.page = "Home"
+                streamlit_app()
 
