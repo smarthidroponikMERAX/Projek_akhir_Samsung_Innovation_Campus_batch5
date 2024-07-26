8a90e20cbbd8e7962ad4afb94f8d51be51038bdf
smarthidroponikMERAX <smarthidroponikmerax@gmail.com>
2024-07-26 22:49:45 +0700

    Update web.py

--- web.py
+++ web.py
@@ -530,25 +530,10 @@ def ml():
       st.line_chart(data_rapi.set_index('waktu')['tds'])
 
 if __name__ == "__main__":
+    streamlit_app()
+    st.sidebar.title("Navigasi")
+    menu = st.sidebar.radio("Pilih Menu", ["Dashboard", "Machine Learning"])
+    if menu == "Dashboard":
         streamlit_app()
-
-        # Menentukan halaman yang aktif
-        if 'page' not in st.session_state:
-            st.session_state.page = "Home"
-        
-        # Definisi halaman Home
-        if st.session_state.page == "Home":
-            st.title("Home")
-            st.write("Ini adalah halaman Home.")
-            if st.button("Lanjutkan ke Page 2"):
-                st.session_state.page = "Page 2"
-        
-        # Definisi halaman Page 2
-        elif st.session_state.page == "Page 2":
-            st.title("Page 2")
-            ml()  # Panggil fungsi ml() di sini
-            st.write("Ini adalah halaman kedua.")
-            if st.button("Kembali ke Home"):
-                st.session_state.page = "Home"
-                streamlit_app()
-
+    elif menu == "Machine Learning":
+        ml()
