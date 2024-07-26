2dfb53f83bb170d674b4d4b12a2bbe62476745fd
smarthidroponikMERAX <smarthidroponikmerax@gmail.com>
2024-07-26 22:24:18 +0700

    Update web.py

--- web.py
+++ web.py
@@ -531,4 +531,22 @@ body {
 
 if __name__ == "__main__":
         streamlit_app()
-        ml()
+
+# Menentukan halaman yang aktif
+        if 'page' not in st.session_state:
+            st.session_state.page = "Home"
+            # Definisi halaman Home
+            if st.session_state.page == "Home":
+                st.title("Home")
+                st.write("Ini adalah halaman Home.")
+                if st.button("Lanjutkan ke Page 2"):
+                    st.session_state.page = "Page 2"
+                    ml()
+            
+            # Definisi halaman Page 2
+            elif st.session_state.page == "Page 2":
+                st.title("Page 2")
+                st.write("Ini adalah halaman kedua.")
+                if st.button("Kembali ke Home"):
+                    st.session_state.page = "Home"
+
